37c2eae7a85b96cd2928fffc1bc34917ca158a36
Souvik Biswas <sbis1999@gmail.com>
2022-07-11 23:49:06 +0530

    Change file name

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_parallax_cards/parallax_cards.dart';
+import 'package:flutter_parallax_cards/travel_cards.dart';
 import 'package:flutter_parallax_cards/res/palette.dart';
 
 class HomePage extends StatefulWidget {
